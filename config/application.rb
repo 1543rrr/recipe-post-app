5c125e3b9e9e304a40e724711978d3ca8c4c282b
1543rrr <73112533+1543rrr@users.noreply.github.com>
2021-05-10 22:20:13 +0900

    Rspecテスト準備

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,18 @@ Bundler.require(*Rails.groups)
 module RecipePostApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.generators do |g|
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      end
   end
 end
